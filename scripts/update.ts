dd3efdf4630211a5488abdcdde6843d776912d94
zhenbo-hu <zhenbohu.hitsz@gmail.com>
2023-05-14 15:13:21 +0800

    remove unused name

--- scripts/update.ts
+++ scripts/update.ts
@@ -1,13 +1,12 @@
-// scripts/upgrade_box.js
 const { ethers, upgrades } = require("hardhat");
 require('dotenv').config();
 
 const SharingNFTContractAddress = process.env.FANTOM_TEST_SHARING_NFT_ADDRESS;
 
 async function main() {
-  const BoxV2 = await ethers.getContractFactory("SharingNFT");
+  const SharingNFT = await ethers.getContractFactory("SharingNFT");
   console.log("Upgrading SharingNFT...");
-  await upgrades.upgradeProxy(SharingNFTContractAddress, BoxV2);
+  await upgrades.upgradeProxy(SharingNFTContractAddress, SharingNFT);
   console.log("SharingNFT upgraded");
 }
 
